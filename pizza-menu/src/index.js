bf25b133a4608e27d2622fb6112b818bd0af2ab1
tuananh582 <101263575+tuananh582@users.noreply.github.com>
2023-08-17 23:48:01 +0700

    new

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -69,16 +69,22 @@ const pizzaData = [
   const pizzas= pizzaData;
   const numPizza= pizzas.length;
   return(
+    
+
 
    <main className='menu'>
    <h2>Our menu</h2>
    {/* {numPizza&&<ul className='pizzas'>
       {pizzaData.map(pizza=><Pizza pizzaObj={pizza} key={pizza.name} />)}
-   </ul>
-   } */}
-   {numPizza>0?( <ul className='pizzas'>
+      </ul>
+    } */}
+   {numPizza>0?(
+       <>
+    <p>Authentic Italian cuisine. 6 creative dishes to choose from.All from our stone oven, all organic, all delicious</p>
+     <ul className='pizzas'>
       {pizzaData.map(pizza=><Pizza pizzaObj={pizza} key={pizza.name} />)}
    </ul>
+   </>
    ):<p>We're still working on our menu. Please come back later</p>}
    {/* <Pizza name='Pizza Spinaci' ingredients='Tomato, mozarella, spinach, and ricotta cheese' 
    photoName='./pizzas/spinaci.jpg' price={10}/>
@@ -87,15 +93,15 @@ const pizzaData = [
    </main>
   )
 }
-function Pizza(props){
-  if(props.pizzaObj.soldOut) return null;
-  console.log(props)
-   return <li className='pizza'>
-       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+function Pizza({pizzaObj}){ // Destructoring props
+  // if(pizzaObj.soldOut) return null;
+   return <li className={`pizza ${pizzaObj.soldOut ?'sold-out':''}`} >
+       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
        <div>
-   <h3>{props.pizzaObj.name}</h3>
-   <p>{props.pizzaObj.ingredients}</p>
-   <span>{props.pizzaObj.price}</span>
+   <h3>{pizzaObj.name}</h3>
+   <p>{pizzaObj.ingredients}</p>
+   {}
+   <span>{pizzaObj.soldOut ? 'Sold Out': pizzaObj.price}</span>
    </div>
    </li>
 }
@@ -103,7 +109,7 @@ function Pizza(props){
   const hour = new Date().getHours();
   const openHour= 12;
   const closeHour=22;
-  const isOpen = hour<=openHour&& hour<=closeHour
+  const isOpen = hour>=openHour&& hour<=closeHour
   // if(hour>=openHour&&hour<=closeHour) alert("we're curretly open ! ")
   // else alert("Sorry we're closed")
 
@@ -117,21 +123,20 @@ function Pizza(props){
     return(
       <footer className='footer'>
         {isOpen?(
-          <Order closeHour={closeHour} />
+          <Order closeHour={closeHour} openHour={openHour} />
         ):<p>We're happy to welcome you between {openHour}:00 and {closeHour}:00 </p>}
       </footer>
     )
     /* //  <footer>{new Date().toLocaleTimeString()}.We're currently open !</footer> */
   }
 
-  function Order(props){
+  function Order({closeHour , openHour}){
       return <div className='order'>
-         
-        <p>We're open until {props.closeHour}:00. Come vist us or ordere online</p>
+        <p>We're open until {openHour}:00am to {closeHour}:00pm. Come vist us or ordere online</p>
         <button className='btn'>Order</button>
-        
          </div>
   }
+
 
 
 
