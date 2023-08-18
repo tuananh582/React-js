459de957ea9e292c6a1b5e02f529217c218c4b92
tuananh582 <101263575+tuananh582@users.noreply.github.com>
2023-08-18 15:53:59 +0700

    new

--- stateEventForms/travel-list/src/App.js
+++ stateEventForms/travel-list/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -22,16 +23,40 @@ function Logo(){
 }
 
 function Form(){
-  return <div className="add-form">
+  const [description,setDescription]=useState("");
+  const [quantity,setQuantity]=useState(1);
+
+
+  function handleSubmit(e){
+      e.preventDefault();
+      if(!description) return;
+
+    const newItem = {description,quantity,packed:false, id:Date.now()};
+    console.log(newItem)
+    setDescription('')
+    setQuantity(1)
+  }
+
+
+  return <form className="add-form" onSubmit={handleSubmit}>
     <h3>What do u need for u 😍</h3>
-  </div>
+    <select value={quantity} onChange={e=>setQuantity(Number(e.target.value))} >
+      {Array.from({length:20},(_,i)=>i+1).map(num=>(
+        <option value={num} key={num}>{num}</option>
+      ))}
+    </select>
+    <input type="text" placeholder="Item..." value={description} 
+    onChange={(e)=>setDescription(e.target.value) } />
+    <button>Add</button>
+   
+  </form>
 }
 
 function PackingList(){
   return(
   <div className="list"> 
   <ul className="list">
-  {initialItems.map(item=><Item item={item}/>)}
+  {initialItems.map(item=><Item item={item} key={item.id}/>)}
   </ul>
   </div>
   )
