128925d2b4379000970bee7e7245876501b1cc7f
tuananh582 <101263575+tuananh582@users.noreply.github.com>
2023-08-19 15:23:38 +0700

    new

--- stateEventForms/steps/src/App.js
+++ stateEventForms/steps/src/App.js
@@ -8,7 +8,15 @@ export default function App(){
   return (
     <div>
       <Steps/>
-      <Steps/>
+      {/* <Steps/> */}
+      <StepMessage step={1}>
+        <p>Pass in Content</p>
+        <p>❗️</p>
+         </StepMessage>
+      <StepMessage step={2}>
+        <p>Read Children Prop</p>
+        <p>😀</p>
+         </StepMessage>
     </div>
   )
 }
@@ -45,11 +53,18 @@ export default function App(){
       <div className={step>=2 ? "active":'' }>2</div>
       <div className={step>=3 ? "active":'' }>3</div>
       </div>
-      <p className="message">Step{step}:{messages[step-1]} {test.name} </p>
+         <StepMessage step={step}>
+         {messages[step-1]}
+         <div className='buttons'>
+         <Button bgColor='#e7e7e7' textColor='#333' onClick={()=>alert(`Learn how to ${messages[step-1]}`)}>Learn how 
+         </Button>
+         </div>
+         </StepMessage>
       <div className="buttons">
-          <button style={{backgroundColor:'#7950f2',color:'#fff'}} onClick={handlePrevious}  >Previous</button>
-          <button style={{backgroundColor:'#7950f2',color:'#fff'}} onClick={handleNext}>Next</button>
- 
+        <Button bgColor='#7950f2' textColor='#fff' onClick={handlePrevious}> <span>🐶</span> Previous </Button>
+        <Button bgColor= '#7950f2' textColor= '#fff' onClick={handleNext} > <span>⛑️</span> Next </Button>
+          {/* <button style={{backgroundColor:'#7950f2',color:'#fff'}} onClick={handleNext}>Next</button> */}
+
       </div>
       </div>
    
@@ -58,4 +73,16 @@ export default function App(){
     </div>
     )
 
+  }
+  function StepMessage({step,children}){
+    return <div className="message">
+    <h3>Step{step}</h3>
+    {children} 
+    </div>
+  }
+  function Button({textColor,bgColor,onClick,children}){
+    return <button>
+          <button style={{backgroundColor:bgColor,color:textColor}} onClick={onClick}>{children} </button>
+
+    </button>
   }
