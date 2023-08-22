d38063498a3cc30fa455a2ee1315fe6116c9387d
tuananh582 <101263575+tuananh582@users.noreply.github.com>
2023-08-22 15:21:04 +0700

    new

--- popcorn/src/index.js
+++ popcorn/src/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 
 import StarRating from './StarRating';
 
+function Test(){
+  const [movierating, setMovieRating] = useState(0)
+  return(
+  <div>
+   <StarRating color='blue' maxRating={10} onSetRating ={setMovieRating} />
+   <p>This movies was rated {movierating} stars</p>
+   </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating maxRating={5} message={['Terrible','Bad','Okay','Good','Amazing']} />
+    <StarRating size={24} color='#a936ad' className="test" defaultRating={1} />
+    <Test/>
   </React.StrictMode>
 );
 
