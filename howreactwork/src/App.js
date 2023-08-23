79eaad125c8a08850d7c72a35b3174213a6a3435
tuananh582 <101263575+tuananh582@users.noreply.github.com>
2023-08-23 15:50:23 +0700

    new

--- howreactwork/src/App.js
+++ howreactwork/src/App.js
@@ -65,7 +65,23 @@ const [likes, setLikes] = useState(0);
 function handleInc() {
   setLikes(likes + 1);
 }
+function handleTripleInc(){
+  // setLikes(likes + 1);
+  // setLikes(likes + 1);
+  // setLikes(likes + 1);
+  setLikes((likes)=>likes+1)
+  setLikes((likes)=>likes+1)
+  setLikes((likes)=>likes+1)
+}
+function handleUndo(){
+  setShowDetails(true);
+  setLikes(0);
+  console.log(likes)
+}
+function handleUndoLater(){
+  setTimeout(handleUndo,2000);
 
+}
 return (
   <div className="tab-content">
     <h4>{item.summary}</h4>
@@ -79,13 +95,13 @@ return (
       <div className="hearts-counter">
         <span>{likes} ❤️</span>
         <button onClick={handleInc}>+</button>
-        <button>+++</button>
+        <button onClick={handleTripleInc}>+++</button>
       </div>
     </div>
 
     <div className="tab-undo">
-      <button>Undo</button>
-      <button>Undo in 2s</button>
+      <button onClick={handleUndo}>Undo</button>
+      <button onClick={handleUndoLater}>Undo in 2s</button>
     </div>
   </div>
   )
